fb5b9e6927535b954b51149ec3c82f398df5ca53
Shubham-padule22 <shubhamp.speroinfosystems@gmail.com>
2025-05-05 17:57:35 +0530

    error resolved
    error resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 // src/App.jsx
 import { useState, useMemo } from "react";
+<<<<<<< Updated upstream
 import { Routes, Route } from "react-router-dom"; // No Router here!
+=======
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+>>>>>>> Stashed changes
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import Sidebar from "./Componenets/Sidebar/Sidebar";
 import Navbar from "./Componenets/Navbar/Navbar";
 import Dashboard from "./Componenets/Dashboard/Dashboard";
+<<<<<<< Updated upstream
 import Login from "./Componenets/Login/Login";
+=======
+>>>>>>> Stashed changes
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
@@ -23,6 +30,7 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
+<<<<<<< Updated upstream
       <div style={{ display: "flex", minHeight: "100vh" }}>
         <Sidebar darkMode={darkMode} />
         <div style={{ flex: 1 }}>
@@ -33,6 +41,17 @@ function App() {
           <Routes>
             <Route path="/Login" element={<Login  darkMode={darkMode} setDarkMode={setDarkMode} />} />
           </Routes>
+=======
+      <Router>
+        <div style={{ display: "flex", minHeight: "100vh" }}>
+          <Sidebar darkMode={darkMode} />
+          <div style={{ flex: 1 }}>
+            <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+            <Routes>
+              <Route path="/Dashboard" element={<Dashboard />} />
+            </Routes>
+          </div>
+>>>>>>> Stashed changes
         </div>
       </div>
     </ThemeProvider>
