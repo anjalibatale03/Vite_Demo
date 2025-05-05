e306c0e45cc84a7b18b00a01ceac2e4fe14b4039
Akshata133 <Akshatam@sperohealthcare.in>
2025-05-05 17:26:32 +0530

    Added Login Page
    Added Login Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,27 @@
-import { BrowserRouter as Router } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Sidebar from "./Componenets/Sidebar/Sidebar";
 import Navbar from "./Componenets/Navbar/Navbar";
+import Login from "./Componenets/Login/Login";
 
 function App() {
 
   return (
-    <Router>
-      {/* <Navbar />
-      <Sidebar /> */}
-      <div style={{ backgroundColor: "black", minHeight: "100vh", color: "white" }}>
+ 
+      <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        {/* Add more routes here */}
+      </Routes>
+{/* 
+      <div style={{ display: "flex", height: "100vh" }}>
         <div style={{ position: "fixed", left: 0, top: 0 }}>
           <Sidebar />
         </div>
-
-        <div style={{ display: "flex", flexDirection: "column" ,}}>
+        <div style={{ display: "flex", flexDirection: "column" }}>
           <Navbar />
         </div>
-      </div>
-    </Router>
+      </div> */}
+    </>
   );
 }
 
