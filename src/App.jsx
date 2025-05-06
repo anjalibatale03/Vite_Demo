08f2a567380edb0bb068d35712b7ba51e059e071
Shubham-padule22 <shubhamp.speroinfosystems@gmail.com>
2025-05-06 11:04:30 +0530

    SOP Module
    Sop Module UI created

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,14 @@
 import { useState, useMemo } from "react";
-import { Routes, Route, useNavigate ,useLocation} from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom"; 
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import Sidebar from "./Componenets/Sidebar/Sidebar";
 import Navbar from "./Componenets/Navbar/Navbar";
-<<<<<<< Updated upstream
-import Footer from "./Componenets/Footer/Footer";
 import Dashboard from "./Componenets/Dashboard/Dashboard";
-=======
-import Dashboard from "./Componenets/SOP/Dashboard";
->>>>>>> Stashed changes
 import Login from "./Componenets/Login/Login";
 import AlertPanel from "./Componenets/SuperAdmin/AlertPanel";
 
 function App() {
   const [darkMode, setDarkMode] = useState(true);
-  const [isLoggedIn, setIsLoggedIn] = useState(false); 
   const location = useLocation();
 
   const theme = useMemo(
@@ -42,7 +36,6 @@ function App() {
             <Route path="/Login" element={<Login darkMode={darkMode} setDarkMode={setDarkMode} />} />
             <Route path="/alert-panel" element={<AlertPanel darkMode={darkMode} setDarkMode={setDarkMode} />} />
           </Routes>
-          <Footer />
         </div>
       </div>
     </ThemeProvider>
